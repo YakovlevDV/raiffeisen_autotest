96fd371fc555026637850bd94f3bd72c4a7358e2
YakovlevDV <barathrum107@gmail.com>
2025-07-07 11:54:23 +0300

    Add currencyNegativeTest

--- src/test/java/tests/CurrencyTest.java
+++ src/test/java/tests/CurrencyTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.hasItems;
 public class CurrencyTest {
 
     @DataProvider
-    public Object[][] currenciesData() {
+    public Object[][] currenciesValidData() {
         return new Object[][] {
                 {"RCONNECT", "USD,EUR,GBP,CHF,JPY,CNY"}, // Интернет банк
                 {"CASH", "USD,EUR"}, // Операционные кассы
@@ -21,8 +21,8 @@ public class CurrencyTest {
         };
     }
 
-    @Test(description = "GET /oapi/currency_rate/get", dataProvider = "currenciesData")
-    public void currencyTest(String source, String currencies) {
+    @Test(description = "GET /oapi/currency_rate/get", dataProvider = "currenciesValidData")
+    public void currencyPositiveTest(String source, String currencies) {
         String schemaPath = "json_schemas/currency_rate.json";
         String[] currenciesList = currencies.split(",");
 
@@ -32,5 +32,25 @@ public class CurrencyTest {
                 .body("data.rates[0].exchange.code.size()", equalTo(currenciesList.length))
                 .body("data.rates[0].exchange.code", hasItems(currenciesList))
                 .body(matchesJsonSchemaInClasspath(schemaPath));
+    }
+
+    @DataProvider
+    public Object[][] currenciesInvalidData() {
+        return new Object[][] {
+                {"", ""}, // Пустые значения source и currencies
+                {"CASHH", "MNT"}, // Невалидные значения source и currencies
+        };
+    }
+
+    @Test(description = "GET /oapi/currency_rate/get", dataProvider = "currenciesInvalidData")
+    public void currencyNegativeTest(String source, String currencies) {
+        String sourceErrorMessage = "The value you selected is not a valid choice.";
+        String currenciesErrorMessage = "One or more of the given values is invalid.";
+
+        CurrencyAPI.perform(source, currencies)
+                .statusCode(400)
+                .body("success", equalTo(false))
+                .body("fields.source", equalTo(sourceErrorMessage))
+                .body("fields.currencies", equalTo(currenciesErrorMessage));
     }
 }
